dbc1364075ad654f9f9bbef9dee090ee53b50dee
devashish1099 <devashish1099@gmail.com>
2020-09-13 22:18:07 +0530

    added Info tab functionality

---
+++ src/components/InfoTable.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <table>
+        <tr v-bind:key="info.date" v-for="info in infos" >
+            <td>{{info.date}}</td>
+            <td>{{info.title}}</td>
+            <td>{{info.link}}</td> 
+            <hr>
+        </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "InfoTable",
+    props: ["infos"],
+    created(){
+        this.infos.forEach(info => {
+            info.date = info.title.slice(0,11);
+            info.title = info.title.slice(11);
+        });
+    }
+}
+</script>
--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="contact">
-    <h1>{{info}}</h1>
+    <Table v-bind:infos="infos"></Table>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Table from  '../components/InfoTable';
 
 export default {
   name : "Info",
+  components : {
+    Table,
+  },
   data(){
     return {
-      info : []
+      infos : []
     }
   },
   created(){
       axios.get('https://api.rootnet.in/covid19-in/notifications')
-      .then(res => this.info = res.data)
+      .then(res => this.infos = res.data.data.notifications)
       .catch(err => console.log(err));
   }
 }
