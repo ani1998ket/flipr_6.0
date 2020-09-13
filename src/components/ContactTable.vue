d0e5e1a4115a868b583d64128dd244744c053c00
aniket <ani1998ket@gmail.com>
2020-09-13 21:52:09 +0530

    Added table tag

---
+++ src/components/ContactTable.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <table>
+        <tr v-bind:key="number.number" v-for="number in numbers" >
+            <td>{{number.loc}}</td>
+            <td>{{number.number}}</td> 
+            <hr>
+        </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "ContactTable",
+    props: ["numbers"]
+}
+</script>
