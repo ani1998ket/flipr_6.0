a5a03f4327ead7151e3fbee1a8c52aa8af18918a
aniket <ani1998ket@gmail.com>
2020-09-13 22:08:14 +0530

    Added dashboard

---
+++ src/components/DashboardTable.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <table>
+            <th>State</th>
+            <th>Rural Hospitals</th>
+            <th>Rural Beds</th>
+            <th>Urban Hospitals</th>
+            <th>Urban Beds</th>
+            <th>Total Hospitals</th>
+            <th>Total Beds</th>
+
+        <tr v-bind:key="row.state" v-for="row in data" >
+            <td>{{row.state}}</td>
+            <td>{{row.ruralHospitals}}</td> 
+            <td>{{row.ruralBeds}}</td>
+            <td>{{row.urbanHospitals}}</td> 
+            <td>{{row.urbanBeds}}</td>
+            <td>{{row.totalHospitals}}</td> 
+            <td>{{row.totalBeds}}</td>
+            <hr>
+        </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "DashboardTable",
+    props: ["data"]
+}
+</script>
